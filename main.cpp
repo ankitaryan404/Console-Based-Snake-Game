6e1442e89c921ec8d90314c0fd0faca2e24f59ce
ankitaryan404 <ankitaryan404@gmail.com>
2024-06-24 14:59:13 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+#include<conio.h>
+#include<Windows.h>
+
+bool gameOver = false;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+void setUp() {
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+
+void draw() {
+	system("cls");
+	for (int i = 0; i <= width + 1; ++i) {
+		std::cout << "#";
+	}
+	std::cout << "\n";
+
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			if (j == 0) {
+				std::cout << "#";
+			}
+			if (i == y && j == x) {
+				std::cout << "O";
+			}
+			else if (i == fruitY && j == fruitX) {
+				std::cout << "F";
+			}
+			else {
+				bool print = false;
+				for (int k = 0; k < nTail; ++k) {
+					if (tailX[k] == j && tailY[k] == i) {
+						std::cout << "o";
+						print = true;
+					}
+				}
+				if(!print)
+					std::cout << " ";
+			}
+			if (j == width - 1) {
+				std::cout << "#\n";
+			}
+		}
+	}
+	for (int i = 0; i <= width + 1; ++i) {
+		std::cout << "#";
+	}
+	std::cout << "\n";
+	std::cout << "Score " << score;
+}
+
+void input() {
+	if (_kbhit()) {
+		switch (_getch()) {
+		case 'a':
+			dir = LEFT;
+			break;
+
+		case 'd' :
+			dir = RIGHT;
+			break;
+
+		case 'w':
+			dir = UP;
+			break;
+
+		case 's':
+			dir = DOWN;
+			break;
+
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
+}
+
+void logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+
+	for (int i = 1; i < nTail; ++i) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+
+	switch (dir) {
+		case LEFT:
+			x--;
+			break;
+
+		case RIGHT:
+			x++;
+			break;
+
+		case UP:
+			y--;
+			break;
+		
+		case DOWN:
+			y++;
+			break;
+	}
+
+	// In case if we want to close the if snake head touches the wall
+	//if (x > width || x < 0 || y > height || y < 0) {
+		//gameOver = true;
+	//}
+
+	if (x >= width) x = 0; else if (x < 0) x = width - 1;
+	if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+	for (int i = 0; i < nTail; ++i) {
+		if (tailX[i] == x && tailY[i] == y) {
+			gameOver = true;
+		}
+	}
+	if (x == fruitX && y == fruitY) {
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		score += 10;
+		nTail++;
+	}
+}
+
+int main() {
+	setUp();
+	while (!gameOver) {
+		draw();
+		input();
+		logic();
+		Sleep(50);
+	}
+}
